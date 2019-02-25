3e9436177058c87b3ebde2c8f51ad9b9f646b682
Frazer Findlater <frazer.findlater@mirumagecny.com>
2019-02-25 17:07:56 +0000

    Adding slick, menu function and other small bits and pieces.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,9 +5,39 @@
   $(document).ready(function(){
     $('.hamburger').click(function(){
       $(this).toggleClass('open');
-      $(".mobile-menu").fadeToggle();
+      $('.mobile-menu').fadeToggle();
       $('body').toggleClass('overflow-hidden');
     });
+  });
+
+  $("a[href^='#']").click(function(e) {
+    e.preventDefault();
+    
+    var position = $($(this).attr('href')).offset().top;
+  
+    $('body, html').animate({
+      scrollTop: position
+    }, 1000 );
+    $('.mobile-menu').fadeToggle();
+    $('body').toggleClass('overflow-hidden');
+    $('.hamburger').toggleClass('open');
+  });
+
+  $(document).ready(function(){
+    $('.home-carousel').slick({
+      arrows: false,
+      infinite: true,
+      speed: 1000,
+      fade: true,
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 10000,
+      pauseOnHover: false
+    });
+  });
+
+  $(function() {
+    $('.lazy').Lazy();
   });
 
 })(jQuery, window, document);
